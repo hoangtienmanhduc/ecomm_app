9ae360e9193caf1dd3cc930a7731f1406147301c
Hoang-Tien-Manh-Duc <ducdeveloper@1201@gmail.com>
2025-03-19 15:21:46 +0700

    chore: add divise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admins
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,5 +12,9 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
+
+  authenticated :admin_user do
+    root to: "admin#index", as: :admin_root
+  end
 end
